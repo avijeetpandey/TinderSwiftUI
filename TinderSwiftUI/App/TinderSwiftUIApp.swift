ef8676f96e9d739274724fe2d31b7d4a72139317
avijeet <avijeetpandey25@gmail.com>
2024-05-07 17:59:22 +0530

    Add: Init folder

--- TinderSwiftUI/App/TinderSwiftUIApp.swift
+++ TinderSwiftUI/App/TinderSwiftUIApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct TinderSwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RootView()
         }
     }
 }
--- TinderSwiftUI/RootView.swift
+++ TinderSwiftUI/RootView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct RootView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    RootView()
 }
