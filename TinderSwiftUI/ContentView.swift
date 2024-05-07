47b6d50c310b54389851e7b40afa929c134a9a26
avijeet <avijeetpandey25@gmail.com>
2024-05-07 17:55:45 +0530

    Initial Commit

---
+++ TinderSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TinderSwiftUI
+//
+//  Created by Avijeet on 07/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TinderSwiftUI/TinderSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderSwiftUIApp.swift
+//  TinderSwiftUI
+//
+//  Created by Avijeet on 07/05/24.
+//
+
+import SwiftUI
+
+@main
+struct TinderSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
