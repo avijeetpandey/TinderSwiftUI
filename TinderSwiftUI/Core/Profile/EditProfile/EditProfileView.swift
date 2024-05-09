32177bf02f89798b6b14394d16f1535701567a3c
avijeet <avijeetpandey25@gmail.com>
2024-05-09 11:20:02 +0530

    Add: Edit profile screen init

---
+++ TinderSwiftUI/Core/Profile/EditProfile/EditProfileView.swift
@@ -0,0 +1,19 @@
+//
+//  EditProfileView.swift
+//  TinderSwiftUI
+//
+//  Created by Avijeet on 09/05/24.
+//
+
+import SwiftUI
+
+// MARK: - EditProfileView
+struct EditProfileView: View {
+    var body: some View {
+        Text("Edit Profile view")
+    }
+}
+
+#Preview {
+    EditProfileView()
+}
